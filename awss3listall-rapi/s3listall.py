13014d4f04bb1b9f9de75ba8bedf1a0a6fdefed8
alfonsof <alfonsofb@gmail.com>
2018-08-17 17:24:53 +0200

    First commit

---
+++ awss3listall-rapi/s3listall.py
@@ -0,0 +1,29 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+# s3listall.py
+# s3listall is an example that handles S3 buckets on AWS
+# It uses Resource API (high-level) of Boto3
+# List information about all S3 buckets and the files they contain
+
+import sys
+import boto3
+import botocore
+
+def main():
+
+# Instantiate the service resource object
+  s3resource = boto3.resource('s3')
+
+  print 'Listing S3 buckets and objects ...'
+  # List buckets
+  for bucket in s3resource.buckets.all():
+    print '* Bucket: ' + bucket.name
+    for s3object in bucket.objects.all():
+      print ' - Object: ' + s3object.key
+
+  return
+
+
+# This is the standard boilerplate that calls the main() function.
+if __name__ == '__main__':
+  main()
