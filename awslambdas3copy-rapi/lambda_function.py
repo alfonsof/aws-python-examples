808fe3749707f9e3dddf38067b8b332270dbb967
alfonsof <alfonsofb@gmail.com>
2018-10-09 10:54:20 +0200

    First commit

---
+++ awslambdas3copy-rapi/lambda_function.py
@@ -0,0 +1,43 @@
+# lamdda_function.py
+# It handles an AWS Lambda function that copies an object when it appears
+# in a S3 bucket to another S3 bucket.
+# It uses Resource API (high-level) of Boto3.
+
+import boto3
+import botocore
+
+# Instantiate the service resource object
+s3resource = boto3.resource('s3')
+
+def lambda_handler(event, context):
+  DESTINATION_BUCKET = 'targetvm'      # Destination bucket name
+
+  source_bucket_name = event['Records'][0]['s3']['bucket']['name']
+  source_key = event['Records'][0]['s3']['object']['key']
+  destination_bucket_name = DESTINATION_BUCKET
+  destination_key = source_key
+  print('From - bucket: ' + source_bucket_name)
+  print('From - object: ' + source_key)
+  print('To   - bucket: ' + destination_bucket_name)
+  print('To   - object: ' + destination_key)
+
+  # Copy the object
+  try:
+    print('Copying object ...')
+    #  Instantiate the destination bucket
+    dest_bucket = s3resource.Bucket(destination_bucket_name)
+    copy_source = {
+      'Bucket': source_bucket_name,
+      'Key': source_key
+    }
+    dest_bucket.copy(copy_source, destination_key)
+    print('Copied')
+  except botocore.exceptions.ClientError as e:
+    if e.response['Error']['Code'] == "AccessDenied":
+      print("Error: Access denied!!")
+    elif e.response['Error']['Code'] == "InvalidBucketName":
+      print("Error: Invalid bucket name!!")
+    elif e.response['Error']['Code'] == "NoSuchBucket":
+      print("Error: No such bucket!!")
+    else:
+      raise
